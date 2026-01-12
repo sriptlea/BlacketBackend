cb516a76dfc1290f89741ac577297cbd3625426e
Script_hacker <xxxxitscoolxxxx@gmail.com>
2026-01-12 15:47:47 -0500

    Update socket.module.ts

--- backend/src/socket/socket.module.ts
+++ backend/src/socket/socket.module.ts
@@ -1,13 +1,12 @@
-import { Global, Module } from "@nestjs/common";
+import { Global, Module, forwardRef } from "@nestjs/common";
 import { SocketGateway } from "./socket.gateway";
 import { SocketService } from "./socket.service";
 import { UsersService } from "src/users/users.service";
 import { ChatModule } from "src/chat/chat.module";
 
-
 @Global()
 @Module({
-    imports: [ChatModule],
+    imports: [forwardRef(() => ChatModule)],
     providers: [SocketGateway, SocketService, UsersService],
     exports: [SocketGateway, SocketService]
 })
